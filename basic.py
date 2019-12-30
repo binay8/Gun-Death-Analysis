c9af0c4714a0b3e5f0716be16be8d780765e431f
Binay Raut <38549341+binay8@users.noreply.github.com>
2019-12-30 14:42:16 -0800

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,73 @@
+#%% 
+import pandas as pd
+
+#%%
+file_path = r"C:\Users\v-birau\OneDrive\Data Analysis Project\Gun Death\By Year_Underlying Cause of Death, 1999-2017.txt"
+
+df = pd.read_csv(file_path,sep='\t')
+df
+
+
+
+# %%
+df.head()
+
+# %%
+df = df.drop(columns = ['Notes', 'State Code','Year Code','Crude Rate'])
+
+# %%
+df = df.dropna(how = "all")
+
+# %%
+df.head(20)
+
+# %%
+base_df = df.groupby(['Year','State']).sum().reset_index()
+
+# %%
+base_df.head(10)
+
+# %%
+by_race_file_path = r"C:\Users\v-birau\OneDrive\Data Analysis Project\Gun Death\By Race_Underlying Cause of Death, 1999-2017.txt"
+
+df_byRace = pd.read_csv(by_race_file_path, sep= '\t')
+# %%
+df_byRace
+
+# %%
+df_byRace = df_byRace.drop(columns = ['Notes', 'State Code','Year Code','Population','Crude Rate','Race Code'])
+
+
+# %%
+df_byRace = df_byRace.dropna(how= 'all')
+
+# %%
+df_byRace.head(10)
+
+# %%
+df_byRace.Race.unique()
+
+# %%
+df_byRace.groupby(['Year','State']).sum().reset_index()
+
+
+# %%
+
+df_byRace[df_byRace['State'] == "Alaska"]
+
+# %%
+base_df
+
+# %%
+df.loc[df['State']== 'Alaska']
+
+#%%
+
+# %%
+base_df['Black or African American_Death'] 
+
+#%%
+
+df_byRace[df_byRace['Year'] == 1999.0][df_byRace["State"] == "Alabama"][df_byRace['Race']=='Black or African American']['Deaths']
+
+# %%
